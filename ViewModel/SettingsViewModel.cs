5aae3d52134f45413410324ca947d4cd65539274
Dr-Electron <kevin199628@hotmail.de>
2020-08-11 23:17:30 +0200

    Add server-status

--- ViewModel/SettingsViewModel.cs
+++ ViewModel/SettingsViewModel.cs
@@ -169,9 +169,104 @@ namespace ElectricShimmer.ViewModel
             }
         }
 
+        public ICommand GetServerStatusCommand { get; set; }
+        public void GetServerStatus(object obj)
+        {
+            try
+            {
+                ProcessStartInfo startinfo = new ProcessStartInfo()
+                {
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    FileName = cliWallet,
+                    Arguments = "server-status",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                };
+
+                Process process = new Process() { StartInfo = startinfo };
+                process.EnableRaisingEvents = true;
+                string output = "";
+                string error = "";
+                process.OutputDataReceived += (sender, args) =>
+                {
+                    output += args.Data + Environment.NewLine;
+                    Log.Write(args.Data, LogLevel.INFO);
+                };
+                process.ErrorDataReceived += (sender, args) =>
+                {
+                    error += args.Data + Environment.NewLine;
+                    Log.Write(args.Data, LogLevel.ERROR);
+                };
+                process.Exited += (sender, args) =>
+                {
+                    process.Dispose();
+                    if (error.Contains("wallet"))
+                    {
+                        Application.Current.Dispatcher.Invoke(async () =>
+                        {
+                            MessageBoxResult result = (MessageBoxResult)await DialogHost.Show(new Controls.MaterialMessageBox("Please generate a seed in the settings tab first", "No seed found", MessageBoxButton.OK), "MainDialogHost");
+                        });
+                    }
+                    else if (error.Contains("no such host"))
+                    {
+                        Application.Current.Dispatcher.Invoke(async () =>
+                        {
+                            MessageBoxResult result = (MessageBoxResult)await DialogHost.Show(new Controls.MaterialMessageBox("Please check your node address.", "Host not found", MessageBoxButton.OK), "MainDialogHost");
+                        });
+                    }
+                    else if (error.Contains("refused"))
+                    {
+                        Application.Current.Dispatcher.Invoke(async () =>
+                        {
+                            MessageBoxResult result = (MessageBoxResult)await DialogHost.Show(new Controls.MaterialMessageBox("Please check your node address and if the node ports are configured correctly." + Environment.NewLine + "Do you want to try again?", "Host refused connection", MessageBoxButton.YesNo), "MainDialogHost");
+                            if (result == MessageBoxResult.Yes)
+                                GenerateSeed(null);
+                        });
+                    }
+                    else if (error.Contains("synchronized"))
+                    {
+                        Application.Current.Dispatcher.Invoke(async () =>
+                        {
+                            MessageBoxResult result = (MessageBoxResult)await DialogHost.Show(new Controls.MaterialMessageBox("Do you want to try again?", "Node not synchronized", MessageBoxButton.YesNo), "MainDialogHost");
+                            if (result == MessageBoxResult.Yes)
+                                GenerateSeed(null);
+                        });
+                    }
+                    else if (error.Length <= 2)
+                    {
+                        Application.Current.Dispatcher.Invoke(async () =>
+                        {
+                            string[] lines = output.Split(new char[] {'\n','\r'}, StringSplitOptions.RemoveEmptyEntries);
+                            string str = string.Join(Environment.NewLine, lines.Skip(1).ToArray());
+                            MessageBoxResult result = (MessageBoxResult)await DialogHost.Show(new Controls.MaterialMessageBox(str, "Server Status", MessageBoxButton.OK), "MainDialogHost");
+                        });
+                    }
+                    else //Unspecified error
+                    {
+                        Application.Current.Dispatcher.Invoke(async () =>
+                        {
+                            MessageBoxResult result = (MessageBoxResult)await DialogHost.Show(new Controls.MaterialMessageBox(error, "Error", MessageBoxButton.OK), "MainDialogHost");
+                        });
+                    }
+                };
+
+                Log.Write(process.StartInfo.FileName + " " + process.StartInfo.Arguments, LogLevel.INFO);
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+            }
+            catch (Exception exc)
+            {
+                Log.Write(exc.Message, LogLevel.EXCEPTION);
+            }
+        }
+
         public SettingsViewModel() : base() 
         {
             GenerateSeedCommand = new RelayCommand(GenerateSeed);
+            GetServerStatusCommand = new RelayCommand(GetServerStatus);
         }
     }
 
